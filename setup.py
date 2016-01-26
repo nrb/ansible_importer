3e9c69ae3acd6bd570e8b7f74ecb8d064bc97e9f
Nolan Brubaker <nolan@nbrubaker.com>
2016-01-25 20:40:06 -0500

    Update some PyPI metadata.

--- setup.py
+++ setup.py
@@ -1,15 +1,30 @@
 from setuptools import setup, find_packages
 
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+
 setup(
     name='ansible_importer',
-    version='0.0.1',
+    version='0.0.2',
     description='Small library to allow Ansible plugins/modules to be imported.',
+    long_description=readme,
+    license='GPLv3',
+    url='https://github.com/nrb/ansible_importer',
     author='Nolan Brubaker',
     author_email='palendae@gmail.com',
+    package_data={'': ['LICENSE']},
     packages=find_packages(),
     py_modules=[
         'ansible_importer',
     ],
     include_package_data=True,
     zip_safe=True,
+    classifiers=(
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+
+    )
 )
